2fca552fee48150e0a54a9c57653df1af5d7cc5e
Vanxh <vanxhyt@gmail.com>
2021-12-31 14:12:34 +0530

    more comments

--- lib/resources/fortnite_profile_ids.dart
+++ lib/resources/fortnite_profile_ids.dart
@@ -2,12 +2,27 @@
 
 /// Fortnite profile id enum
 enum FortniteProfile {
+  /// save the world profile
   campaign,
+
+  /// battle royale profile
   athena,
+
+  /// common core (purchases) profile
   common_core,
+
+  /// public profile
   common_public,
+
+  /// save the world backpack profile
   theater0,
+
+  /// save the world events backpack profile
   theater1,
+
+  /// save the world ventures backpack profile
   theater2,
+
+  /// save the world storage backpack profile
   outpost0,
 }
